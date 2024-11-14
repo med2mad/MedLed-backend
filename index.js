56b4ef99ccfefb1717328d13bf52c41a03f11195
mohamed leghdaich <113247036+med2mad@users.noreply.github.com>
2024-11-14 01:18:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const app = require('express')();
+const io = require('socket.io')(4000, {cors:'*'})
+const dialogflow = require('@google-cloud/dialogflow');
+const path = require('path');
+const cors = require('cors');
+
+const sessionClient = new dialogflow.SessionsClient({
+  keyFilename: path.join(__dirname, 'credentials.json')
+});
+
+io.on("connection", (socket)=>{
+    socket.on('join', (username, userroom, userphoto)=>{
+        socket.name = username
+        socket.photo = userphoto
+        socket.room = userroom
+
+        socket.join(userroom)
+        socket.to(userroom).emit('receive', `${socket.name} joined`, socket.id, socket.photo)
+    })
+
+    socket.on('send', (message)=>{
+        socket.to(socket.room).emit('receive', message, socket.id, socket.photo)
+    })
+
+    socket.on('disconnect', () => {
+        socket.to(socket.room).emit('receive', `${socket.name} disconnected`, socket.id, socket.photo)
+    });
+})
+
+
+// dialogflowClient.js ////////////////////////////////////
+
+
+async function detectIntent(sessionId, text) {
+  const sessionPath = sessionClient.projectAgentSessionPath('projectid-4444', sessionId);
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: text,
+        languageCode: 'en',
+      },
+    },
+  };
+
+  const [response] = await sessionClient.detectIntent(request);
+  const result = response.queryResult;
+  return result.fulfillmentText;
+}
+
+app.use(cors());
+app.get('/', async(req, res)=>{
+  try {
+    const botResponse = await detectIntent('sessionId', req.query.message);
+    res.json({answer:botResponse});
+  } catch (error) {console.log('errror : ' + error); res.send('errror : ' + error);}
+});
+
+app.listen(5000, ()=>{console.log('listening on : 5000')});
